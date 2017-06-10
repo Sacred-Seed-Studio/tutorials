e98b737b053ec4a16ee61cd4eee97d87359adab0
Joel Kuntz <joel.kuntz.92@gmail.com>
2017-06-10 12:07:20 -0300

    [T001] Exporting Data

---
+++ interacting-with-json-in-unity/Assets/ExportingData/ExportingData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class ExportingData : MonoBehaviour
+{
+    public Post post;
+
+    void Start()
+    {
+        StartCoroutine(CreatePost());
+    }
+
+    IEnumerator CreatePost()
+    {
+        Dictionary<string, string> headers = new Dictionary<string, string>();
+        headers.Add("Content-Type", "application/json");
+
+        byte[] postData = Encoding.ASCII.GetBytes(JsonUtility.ToJson(post));
+
+        WWW www = new WWW(API.baseURL + "/posts", postData, headers);
+
+        yield return www;
+
+        Debug.Log("Created a Post: " + www.text);
+        post = JsonUtility.FromJson<Post>(www.text);
+    }
+}
