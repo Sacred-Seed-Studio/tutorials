019ba2fdb8a289dc88bf678110d63ee452da241f
Joel Kuntz <joel.kuntz.92@gmail.com>
2017-06-10 11:40:35 -0300

    [T001] Simple Example

---
+++ interacting-with-json-in-unity/Assets/SimpleExample/SimpleExample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SimpleExample : MonoBehaviour
+{
+    public User user;
+    public Post post;
+
+    void Start()
+    {
+        StartCoroutine(LoadPost());
+        StartCoroutine(LoadUser());
+    }
+
+    IEnumerator LoadPost()
+    {
+        WWW www = new WWW(API.baseURL + "/posts/1");
+        yield return www;
+        post = JsonUtility.FromJson<Post>(www.text);
+        Debug.Log("Post: " + www.text);
+    }
+
+    IEnumerator LoadUser()
+    {
+        WWW www = new WWW(API.baseURL + "/users/1");
+        yield return www;
+        user = JsonUtility.FromJson<User>(www.text);
+        Debug.Log("User: " + www.text);
+    }
+}
