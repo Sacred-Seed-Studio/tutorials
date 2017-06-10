44a5c7efd617c835f2d07c5b233132c4d43b79b2
Joel Kuntz <joel.kuntz.92@gmail.com>
2017-06-10 12:00:30 -0300

    [T001] Handling Arrays

---
+++ interacting-with-json-in-unity/Assets/HandlingArrays/HandlingArrays.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandlingArrays : MonoBehaviour
+{
+    public User[] users;
+
+    void Start()
+    {
+        StartCoroutine(LoadUsers());
+    }
+
+    IEnumerator LoadUsers()
+    {
+        WWW www = new WWW(API.baseURL + "/users");
+        yield return www;
+        users = JsonHelper.getJsonArray<User>(www.text);
+        Debug.Log("Users: " + www.text);
+    }
+}
